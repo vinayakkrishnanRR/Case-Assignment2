b35d181aef90f57631ae52dd1be0ceb873ccfaa1
vinayakkrishnanRR <141342532+vinayakkrishnanRR@users.noreply.github.com>
2023-09-26 19:33:10 +0530

    Update signup1.js

--- signup1.js
+++ signup1.js
@@ -1,93 +1,149 @@
-// Function to validate the form
-function validate() {
-    // Get form input values
-    const firstName = document.getElementById("name").value;
-    const secondName = document.getElementById("name").value;
-    const lastName = document.getElementById("name2").value;
-    const state = document.getElementById("State").value;
-    const city = document.getElementById("City").value;
-    const phoneNumber = document.getElementById("phn").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const passwordReentered = document.getElementById("pwdre").value;
+//variable declaration
+    var firstName = document.getElementById("name");
+    var secondName = document.getElementById("name1");
+    var lastName = document.getElementById("name2");
+    var phoneNumber = document.getElementById("phn");
+    var confirmNumber = document.getElementById("numConfirm");
+    var Email = document.getElementById("email");
+    var confirmEmail = document.getElementById("emailConfirm");
+    var password = document.getElementById("password");
+    var passCheck = document.getElementById("passCheck");
+    var passwordReentered = document.getElementById("pwdre");
+    var confirmPassword= document.getElementById("passConfirm");
+    const passStrengthDisplay = document.getElementById("passStrengthDisplay");
+    const form=document.getElementById("signupform")
   
-    // Regular expression for phone number validation
-    const phoneNumberRegex = /^(?:\d{10}|\d{3}[-.\s]\d{3}[-.\s]\d{4})$/;
-  
-    // Regular expression for email validation
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //validation conditions
 
-
-    //  Check if any field is empty
-    if (firstName === '' && secondName===''&& lastName === ''&& state==='' && city===''&& phoneNumber===''&& email === ''  && password === '' && passwordReentered === '') {
-        alert('All fields must be filled out');
-        return false;
-    }
-  
-    // Check email format
-    if (!emailRegex.test(email)) {
-      alert("Please enter a valid email address.");
-      return false;
-    }
-  
-    // Check phone number format
-    if (!phoneNumberRegex.test(phoneNumber)) {
-      alert("Please enter a valid phone number.");
-      return false;
-    }
-  
-    // Check password strength
-  //  const  passwordStrength = zxcvbn(password).score;
-  //   const strengthIndicator = document.getElementById("password-strength");
-  
-  //   if (passwordStrength < 2) {
-  //     strengthIndicator.style.backgroundColor = "red";
-  //     strengthIndicator.textContent = "Weak";
-  //   } else if (passwordStrength < 3) {
-  //     strengthIndicator.style.backgroundColor = "orange";
-  //     strengthIndicator.textContent = "Medium";
-  //   } else {
-  //     strengthIndicator.style.backgroundColor = "green";
-  //     strengthIndicator.textContent = "Strong";
-  //   }
-  
-    // Check password match
-    if (password !== passwordReentered) {
-      alert("Passwords do not match.");
-      return false;
-    }
-  
-    // If all validations pass, submit the form
-    // alert("Form submitted successfully!");
-    return true;
+    function validateMail(){
+      if(Email.value==""){
+        confirmEmail.innerText= "This field cannot be left blank";
+        confirmEmail.style.color= "red";  
+        return false      
+       }else{
+        const emailRegex= /^([a-zA-Z0-9\.-]+)@([a-z]+)\.([a-z]{2,3})(\.[a-z]{2,4})?$/;
+        if(emailRegex.test(Email.value)){
+          confirmEmail.innerText= "E-mail valid";
+          confirmEmail.style.color= "green";
+            return true
+        }else{
+          confirmEmail.innerText= "E-mail invalid";
+          confirmEmail.style.color= "red";
+            return false    
+        }}
+      }
+    
+      function validateNum(){
+        if(phoneNumber.value==""){
+          confirmNumber.innerText= "This field cannot be left blank";
+          confirmNumber.style.color= "red";
+          return false
+      }else{
+          const phoneRegex= /^(\d{10}|\d{3}[-.\s]\d{3}[-.\s]\d{4})$/;
+          if(phoneRegex.test(phoneNumber.value)){
+            confirmNumber.innerText= "Phone Number valid";
+            confirmNumber.style.color= "green";
+              return true
+          }else{
+            confirmNumber.innerText= "Invalid Number or Formatting (Enter numbers separated by . - split as 3|3|4 or without any spacing)";
+            confirmNumber.style.color= "red";
+              return false    
+          }
+      }    
   }
-  function checkPasswordStrength() {
-    const password = document.getElementById("password").value;
-    const passwordStrength = document.getElementById("password-strength");
-    const progressBar = passwordStrength.querySelector(".progress-bar");
 
-    // Calculate password strength score (0 to 4)
-    let strength = 0;
+  const midPassRegex=/^(?=.*[a-zA-Z0-9]){8,}$/;
+const strongPassRegex=/^(?=.*[a-zA-Z0-9])(?=.*[!@#$%&*()_+=\[{\]};:'\",<.>/?~-]).{10,}$/
 
-    if (password.length >= 8) {
-        strength += 1;
-    }
-
-    if (/[A-Z]/.test(password)) {
-        strength += 1;
-    }
-
-    if (/[a-z]/.test(password)) {
-        strength += 1;
-    }
-    progressBar.style.width = (strength / 4) * 100 + "%";
-
-    // Apply color classes based on the score
-    if (strength === 0) {
-        progressBar.className = "progress-bar bg-danger";
-    } else if (strength === 1) {
-        progressBar.className = "progress-bar bg-warning";
-    } else if (strength >= 2) {
-        progressBar.className = "progress-bar bg-success";
+let passStrength= 0;
+function getPassStrength(){
+    passStrength=0;
+    if(strongPassRegex.test(password.value)){
+        passStrength+=2    
+    }if(midPassRegex.test(password.value)){
+        passStrength++
+    }if(password.value.length==8){
+        passStrength++
+    }if(password.value.length>10){
+        passStrength++
+    }if(password.value.length==0){
+        passStrength==0
     }
 }
+
+function validatePass(){
+   getPassStrength();      
+  if( password.value==""){
+    passCheck .innerText= "This field cannot be left blank";
+    passCheck .style.color= "red";
+      passStrengthDisplay.classList.add('weakPassword');        
+      return false;
+  }else if(password.value.length<8){
+    passCheck.innerText= "This password is too weak. At least 8 characters required.";
+    passCheck.style.color= "red";
+      passStrengthDisplay.classList.add('weakPassword');           
+      return false;
+  }else if(passStrength==0){
+      passCheck.innerText= "This password is Weak. Use a mix of numbers, upper and lowercase letters";
+      passCheck.style.color= "red";
+      passStrengthDisplay.classList.add('weakPassword');           
+      return false;
+  }else if(passStrength==1){
+      passCheck.innerText= "Medium password. Use numbers and special characters to make this stronger.";
+      passCheck.style.color= "orange"
+      passStrengthDisplay.classList.add('mediumPassword');     
+      return true;
+  }else if(passStrength==2){
+      passCheck.innerText= "Strong password";
+      passCheck.style.color= "green" 
+      passStrengthDisplay.classList.add('strongPassword');          
+      return true;
+  }else if(passStrength>=3){
+      passCheck.innerText= "Very strong password";
+      passCheck.style.color= "green" 
+      passStrengthDisplay.classList.add('veryStrongPassword');        
+      return true;
+  }else{
+      passCheck.innerText= "Invalid password";
+      passCheck.style.color= "red";
+      passStrengthDisplay.classList.add('weakPassword');   
+      return false
+  }
+}
+function resetPassBar(){
+  passStrength= 0
+  passStrengthDisplay.classList.remove('weakPassword');  
+  passStrengthDisplay.classList.remove('mediumPassword');  
+  passStrengthDisplay.classList.remove('strongPassword');  
+  passStrengthDisplay.classList.remove('veryStrongPassword');  
+}
+function confirmPass(){
+  if(password.value==""&&passwordReentered.value==""){
+      passCheck.innerText= "Passwords not entered";
+      passCheck.style.color= "red";
+      return false
+  }else if(password.value==passwordReentered.value){
+      passCheck.innerText= "Passwords are a match";
+      passCheck.style.color= "green"
+      return true
+  }else{
+      passCheck.innerText= "Passwords do not match";
+      passCheck.style.color= "red";
+      return false
+  }
+}
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  var validMail=validateMail();
+  var validNum=validateNum(); 
+  var validPass=validatePass(); 
+  var cPass=confirmPass();
+
+  if(validMail===true&&validNum===true&&validPass===true&&cPass===true){
+  form.submit();
+  window.location.href= './success.html';
+  }else{
+      alert("Invalid Format");
+      console.log("alert");
+}
+});
